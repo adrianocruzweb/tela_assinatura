85cb4c14e970baa441123316145463d73815a8d9
Adriano Cruz <adrianocruzweb@gmail.com>
2020-03-15 21:33:06 -0400

    firs comit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import Main from './pages/main';
+
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +21,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Header/>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './styles.css';
+
+const Header = () => <header id="main-header">JS Teste</header>
+
+
+export default Header;
+/* class Header extends Component{
+    render(){
+        return <h1>Hello</h1>
+    }
+} */
