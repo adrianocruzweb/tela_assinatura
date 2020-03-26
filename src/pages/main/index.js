37899e62c2ab268a564d5327380f4af30b564f1a
adrianocruzweb <adrianocruzweb@gmail.com>
2020-03-26 13:36:50 -0400

    listagem e get id na tela

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import api from '../../services/api';
 
+import {Link} from 'react-router-dom';
+
 import './styles.css';
 
 export default class Main extends Component{
@@ -21,7 +23,7 @@ export default class Main extends Component{
         const { docs, ... productInfo } = response.data;
         
         this.setState({ products: docs, productInfo, page});
-    };
+    }; 
 
     prevPage = () => {
         const { page, productInfo} = this.state;
@@ -53,12 +55,12 @@ export default class Main extends Component{
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-                        <a href="">Acessar</a>
+                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
                 <div className="actions">
-                    <button disabled={page===1} onClick={this.prevPage()}>Anterior</button>
-                    <button disabled={page===productInfo.pages} onClick={this.nextPage()}>Próximo</button>
+                    <button disabled={page===1} onClick={()=>this.prevPage()}>Anterior</button>
+                    <button disabled={page===productInfo.pages} onClick={()=>this.nextPage()}>Próximo</button>
                 </div>
             </div>
         )
